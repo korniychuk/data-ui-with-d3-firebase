691077ffa674aacfc84d901947dca9a44561c974
Anton Korniychuk <dev@korniychuk.pro>
2019-03-04 19:50:11 +0200

    money 3 - chart done without legend, animations and event handlers

---
+++ ak/test-d3-money-3/graph.js
@@ -0,0 +1,81 @@
+const dims = { width: 300, height: 300, radius: 150 };
+const cent = { x: (dims.width / 2) + 5, y: (dims.height / 2) + 5 };
+
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', dims.width + 150)
+    .attr('height', dims.width + 150)
+;
+
+const graph = svg.append('g')
+    .attr('transform', `translate(${cent.x}, ${cent.y})`)
+;
+
+const pie = d3.pie()
+    .sort(null)
+    .value(v => v.cost)
+;
+
+// paths vs path
+const arcPath = d3.arc()
+    .outerRadius(dims.radius)
+    .innerRadius(dims.radius / 2)
+;
+
+const colour = d3.scaleOrdinal(d3['schemeSet3']);
+
+const update = (data) => {
+    console.log('update() data:', data);
+
+    // 1. update data
+    const paths = graph.selectAll('path').data(pie(data));
+
+    // 2. update domains
+    colour.domain(data.map(v => v.name));
+
+    // 3. exiting elements
+    paths.exit().remove();
+
+    // 4. updating elements
+    paths
+        .attr('d', arcPath)
+        .attr('stroke', 'white')
+        .attr('stroke-width', 3)
+        .attr('fill', d => colour(d.data.name))
+    ;
+
+    // 5. entering elements
+    paths.enter()
+        .append('path')
+        .attr('d', arcPath)
+        .attr('stroke', 'white')
+        .attr('stroke-width', 3)
+        .attr('fill', d => colour(d.data.name))
+    ;
+
+
+};
+
+let data = [];
+db.collection('expenses').onSnapshot((res) => {
+    res.docChanges().forEach((change) => {
+        const doc = { ...change.doc.data(), id: change.doc.id };
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+
+            case 'modified':
+                const index = data.findIndex(v => v.id === doc.id);
+                data[index] = doc;
+                break;
+
+            case 'removed':
+                data = data.filter(v => v.id !== doc.id);
+                break;
+        }
+
+    });
+    update(data);
+});
