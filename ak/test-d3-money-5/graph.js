add08323832bd501145cb25e1427bde79ae6f8f8
Anton Korniychuk <dev@korniychuk.pro>
2019-03-07 13:31:21 +0200

    money 5 - base chart done

--- ak/test-d3-money-5/graph.js
+++ ak/test-d3-money-5/graph.js
@@ -1,5 +1,37 @@
+const dims = { width: 300, height: 300, radius: 150 };
+const cent = { x: (dims.width / 2) + 5, y: (dims.height / 2) + 5 };
+
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', dims.width + 150)
+    .attr('height', dims.height + 150)
+;
+
+const graph = svg.append('g').attr('transform', `translate(${cent.x}, ${cent.y})`);
+
+const pie = d3.pie()
+    .sort(null)
+    .value(v => v.cost)
+;
+const arcPath = d3.arc()
+    .outerRadius(dims.radius)
+    .innerRadius(dims.radius / 2)
+;
+
 const update = (data) => {
     console.log('update() data:', data);
+
+    // 1.
+    const paths = graph.selectAll('path').data(pie(data));
+
+    // 5.
+    paths.enter()
+        .append('path')
+        .attr('d', arcPath)
+        .attr('stroke', 'white')
+        .attr('stroke-width', 3)
+        .attr('fill', 'black')
+    ;
 };
 
 let data = [];
@@ -22,5 +54,6 @@ db.collection('expenses').onSnapshot(res => {
         }
 
     });
+
     update(data);
 });
