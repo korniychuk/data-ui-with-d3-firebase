2eaf2a0da2c280cf7f487eee8bef044175403b9f
anton.korniichuk <anton.korniichu@e-hps.com>
2019-03-08 18:55:48 +0200

    fitness 0 - data fetching done

--- ak/fitnes-0/graph.js
+++ ak/fitnes-0/graph.js
@@ -1,1 +1,27 @@
+const update = (data) => {
+    console.log('update() data:', data);
+};
 
+let data = [];
+db.collection('activities').onSnapshot(res => {
+
+    res.docChanges().forEach(change => {
+        const doc = { ...change.doc.data(), id: change.doc.id };
+        switch (change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+
+            case 'modified':
+                const index = data.findIndex(v => v.id === doc.id);
+                data[index] = doc;
+                break;
+
+            case 'removed':
+                data = data.filter(v => v.id !== doc.id);
+                break;
+        }
+    });
+
+    update(data);
+});
